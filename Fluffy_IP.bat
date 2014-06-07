157f61cdb5d4298081a589e34a7ba5d1564db180
ryanbacon <ryan@thebacons.us>
2014-06-07 12:21:26 -0600

    Changed IP settings

--- Fluffy_IP.bat
+++ Fluffy_IP.bat
@@ -1,1 +1,5 @@
 netsh int ipv4 set address name="Wireless Network Connection" source=static address=10.42.93.7 mask=255.0.0.0 gateway=
+==IPs 1-6 Reserved for FIRST==
+==IP 7 - Ryan Bacon==
+==IP 8 - Cody Clayton==
+
